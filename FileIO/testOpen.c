db654ac68906860747241e7aeea8fbbcc1b426d7
MMUWEI <1821370389@qq.com>
2024-01-04 16:17:26 +0800

    [修改] 文件打开不存在则新建；关闭文件

--- FileIO/testOpen.c
+++ FileIO/testOpen.c
@@ -3,16 +3,19 @@
 #include <sys/stat.h>
 #include <fcntl.h>
 #include <errno.h>
-
+#include <unistd.h>
 
 int main()
 {
     /* 打开文件 可读可写 */
-    int fd = open("./hello.txt", O_RDWR, 0644);
+    int fd = open("./hello.txt", O_RDWR | O_CREAT, 0644);
     if(fd == -1)
     {
         perror("open error");
     }
+    
+    /* 关闭文件 */
+    close(fd);
 
     return 0;
 }
