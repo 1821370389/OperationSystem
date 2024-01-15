4c16573096c7bcae7684929b6f14f3633d6d469f
MMUWEI <1821370389@qq.com>
2024-01-15 15:33:54 +0800

    [修改] 新增线程池中任务队列的属性

--- threadPoll/threadPoll.h
+++ threadPoll/threadPoll.h
@@ -3,17 +3,33 @@
 
 #include <pthread.h>
 
+/* 任务结构体 */
+typedef struct task_t
+{
+    void *(*function)(void *);
+    void *arg;
+    struct task_t *next;
+    pthread_mutex_t mutex;
+    pthread_cond_t cond;
+}task_t;
+
 /* 线程池结构体 */
 typedef struct thread_poll_t 
 {
-    pthread_t *thread_id;   // 线程id数组
-    int max_thread_num;     // 最大线程数量
-    int min_thr_num;        // 最小线程数量
+    task_t *taskQueue;     // 任务队列
+    int queueCapacity;        // 任务队列容量
+    int queueSize;            // 任务队列大小
+    int queueFront;           // 队列头
+    int queueRear;            // 队列尾
+
+    pthread_t *threadID;   // 线程id数组
+    int maxSize;        // 最大线程数量
+    int minSize;        // 最小线程数量
 }
 thread_poll_t;
 
 /* 线程池初始化 */
-int thread_poll_init(thread_poll_t *thread_poll, int min_thr_num, int max_thr_num);
+int thread_poll_init(thread_poll_t *thread_poll, int minSize, int maxSize, int queue_max_size);
 /* 线程池销毁 */
 int thread_poll_destroy(thread_poll_t *thread_poll);
 /* 线程池启动 */
