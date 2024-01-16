55b68ea401c439adc7ad9361007c703f851ff534
MMUWEI <1821370389@qq.com>
2024-01-16 11:38:39 +0800

    [修改] 线程池demo

--- threadPoll/threadPoll.c
+++ threadPoll/threadPoll.c
@@ -9,6 +9,8 @@
 #define DEFAULT_MIN_THREAD_NUM 0
 #define DEFAULT_MAX_THREAD_NUM 10
 #define DEFAULT_QUEUE_MAX_SIZE 100
+#define TIME_INTERVAL 5
+#define DEFAULT_EXPANSION_SIZE 3
 
 /* 状态码 */
 enum STATUS_CODE
@@ -22,14 +24,147 @@ enum STATUS_CODE
     
 };
 
-/* 线程函数 */
-void *threadHander(void *arg)
+/* 静态函数前置声明 */
+static void *threadHander(void *arg);
+static void *mangerHander(void *arg);
+
+/* 线程退出清除资源 */
+static int threadExitClrResources(thread_poll_t *thread_poll)
 {
-    
+    for(int idx = 0; idx < thread_poll->maxSize; idx++)
+    {
+        if(thread_poll->threadID[idx] == pthread_self())
+        {
+            thread_poll->threadID[idx] = 0;
+            break;
+        }
+    }
+    pthread_exit(NULL);
+}
+
+/* 线程函数 */
+static void *threadHander(void *arg)
+{
+    thread_poll_t *thread_poll = (thread_poll_t *)arg;
+    while(1)
+    {
+        /* 加锁 */
+        pthread_mutex_lock(&thread_poll->mutex);
+        while(thread_poll->queueSize == 0)
+        {
+            /* 等待一个条件变量被唤醒 */
+            pthread_cond_wait(&thread_poll->notEmpty, &thread_poll->mutex);
+            if(thread_poll->exitSize > 0)
+            {
+                thread_poll->exitSize--;
+                /* 解锁 */
+                pthread_mutex_unlock(&thread_poll->mutex);
+                if(thread_poll->aliveSize > thread_poll->minSize)
+                {
+                    /* 释放资源 */
+                    threadExitClrResources(thread_poll);
+                    /* 退出 */
+                    return NULL;
+                }
+            }
+        }
+        /* 出队 */
+        task_t task = thread_poll->taskQueue[thread_poll->queueFront]; 
+        thread_poll->queueFront = (thread_poll->queueFront + 1) % thread_poll->queueCapacity;
+        /* 队列大小-1 */
+        thread_poll->queueSize--;
+        /* 解锁 */
+        pthread_mutex_unlock(&thread_poll->mutex);
+        /* 唤醒生产者 */
+        pthread_cond_signal(&thread_poll->notFull);
+
+        /* 为了提升性能，在创建一把只维护busySize的锁 */
+        pthread_mutex_lock(&thread_poll->busyMutex);
+        /* 增加busySize */
+        thread_poll->busySize++;
+        /* 解锁 */
+        pthread_mutex_unlock(&thread_poll->busyMutex);
+
+        /* 执行任务 */
+        task.function(task.arg);
+        /* 释放任务 */
+        free(task.arg);
+        task.arg = NULL;
+        free(task.function);
+        task.function = NULL;
+
+        pthread_mutex_lock(&thread_poll->busyMutex);
+        /* 减少busySize */
+        thread_poll->busySize--;
+        /* 解锁 */
+        pthread_mutex_unlock(&thread_poll->busyMutex);
+
+        
+
+    }
+}
+
+static void *mangerHander(void *arg)
+{
+    thread_poll_t *thread_poll = (thread_poll_t *)arg;
+    while(1)
+    {
+        sleep(TIME_INTERVAL);
+
+        /* 获取线程池信息 */
+        pthread_mutex_lock(&thread_poll->mutex);
+        int taskNum = thread_poll->queueSize;
+        int aliveSize = thread_poll->aliveSize;
+        pthread_mutex_unlock(&thread_poll->mutex);
+
+        pthread_mutex_lock(&thread_poll->busyMutex);
+        int busySize = thread_poll->busySize;
+        pthread_mutex_unlock(&thread_poll->busyMutex);
+
+        /* 扩容: 任务数>存活线程数 && 存活线程数<最大线程数  */
+        if(taskNum > aliveSize && aliveSize < thread_poll->maxSize)
+        {
+            /* 一次扩大3个 */
+            int count = 0;
+            int ret = 0;
+            pthread_mutex_lock(&thread_poll->mutex);
+            for(int idx = 0; count < DEFAULT_EXPANSION_SIZE && 
+            idx < thread_poll->maxSize && aliveSize <= thread_poll->maxSize; idx++)
+            {
+                if(thread_poll->threadID[idx] == 0)
+                {
+                    ret = pthread_create(&thread_poll->threadID[idx], NULL, threadHander, thread_poll);
+                    if(ret != 0)
+                    {
+                        perror("pthread_create error");
+                        break;/* todo */
+                    }
+                    count++;
+                    thread_poll->aliveSize++;
+                }
+            }
+            pthread_mutex_unlock(&thread_poll->mutex);
+        }
+        
+        /* 缩容: 忙线程数*2>存活线程数 && 存活线程数>最小线程数 */
+        if((busySize << 1) > aliveSize && aliveSize > thread_poll->minSize)
+        {
+            pthread_mutex_lock(&thread_poll->mutex);
+
+            thread_poll->exitSize = DEFAULT_EXPANSION_SIZE;
+            for(int idx = 0; idx < DEFAULT_EXPANSION_SIZE; idx++)
+            {
+                pthread_cond_signal(&thread_poll->notEmpty);
+            }
+            pthread_mutex_unlock(&thread_poll->mutex);
+        }
+
+
+    }
 }
 
 /* 线程池初始化 */
-int thread_poll_init(thread_poll_t *thread_poll, int minSize, int maxSize, int queueCapacity)
+int threadPollInit(thread_poll_t *thread_poll, int minSize, int maxSize, int queueCapacity)
 {
     if(thread_poll == NULL)
     {
@@ -49,6 +184,8 @@ int thread_poll_init(thread_poll_t *thread_poll, int minSize, int maxSize, int q
         /* 更新线程池属性 */
         thread_poll->minSize = minSize;
         thread_poll->maxSize = maxSize;
+        thread_poll->busySize = 0;
+        
 
         /* 队列大小合法性 */
         if(queueCapacity <= 0)
@@ -79,13 +216,20 @@ int thread_poll_init(thread_poll_t *thread_poll, int minSize, int maxSize, int q
         meset(thread_poll->threadID, 0, sizeof(pthread_t) * maxSize);
 
         int ret = 0;
+        /* 创建管理线程 */
+        ret = pthread_create(&thread_poll->managerID, NULL, mangerHander, (void*)thread_poll);
+        if(ret != 0)
+        {
+            perror("pthread_create error");
+            break;
+        }
         /* 创建线程 */
         for(int idx = 0; idx < minSize; idx++)
         {
             /* 判断ID号是否能够使用 */
             if(thread_poll->threadID[idx] == 0)
             {
-                ret = pthread_create((thread_poll->threadID[idx]), NULL, threadHander, NULL);
+                ret = pthread_create((thread_poll->threadID[idx]), NULL, threadHander, (void*)thread_poll);
                 if(ret != 0)
                 {
                     // perror("pthread_create error");
@@ -98,6 +242,22 @@ int thread_poll_init(thread_poll_t *thread_poll, int minSize, int maxSize, int q
         }
         if(ret != 0)
         {
+            break;
+        }
+        /* 存活的线程数等于最小线程数 */
+        thread_poll->aliveSize = minSize;
+        /* 退出线程数 */
+        thread_poll->exitSize = 0;
+        /* 销毁标志 */
+        thread_poll->shutdown = 0;
+
+        /* 锁初始化 */
+        pthread_mutex_init(&(thread_poll->mutex), NULL);
+        pthread_mutex_init(&(thread_poll->busyMutex), NULL);
+        /* 条件变量初始化 */
+        if(pthread_cond_init(&(thread_poll->notEmpty), NULL) != 0 || pthread_cond_init(&(thread_poll->notFull), NULL) != 0)
+        {
+            perror("pthread_cond_init error");
             break;
         }
 
@@ -125,10 +285,66 @@ int thread_poll_init(thread_poll_t *thread_poll, int minSize, int maxSize, int q
         free(thread_poll->threadID);
         thread_poll->threadID = NULL;
     }
+    /* 回收管理者资源 */
+    if(thread_poll->managerID != NULL)
+    {
+        free(thread_poll->managerID);
+        thread_poll->managerID = NULL;
+    }
+
+
+    /* 释放锁资源 */
+    pthread_mutex_destroy(&(thread_poll->mutex));
+    pthread_mutex_destroy(&(thread_poll->busyMutex));
+    /* 释放条件变量资源 */
+    pthread_cond_destroy(&(thread_poll->notEmpty));
+    pthread_cond_destroy(&(thread_poll->notFull));
 
     return UNKNOWN_ERROR;
 }
+
+/* 线程池添加任务 */
+int threadPollAddTask(thread_poll_t *thread_poll, void *(*function)(void *), void *arg)
+{
+    if(thread_poll == NULL || function == NULL || arg == NULL)
+    {
+        return NULL_PTR;
+    }
+
+    /* 加锁 */
+    pthread_mutex_lock(&thread_poll->mutex);
+    /* 任务队列满了 */
+    // while(thread_poll->aliveSize == thread_poll->queueCapacity)
+    while(thread_poll->queueSize == thread_poll->queueCapacity)
+    {
+        pthread_cond_wait(&thread_poll->notFull, &thread_poll->mutex);
+    }
+    /* 到此位置说明队列有空位 */
+    /* 添加任务 */
+    thread_poll->taskQueue[thread_poll->queueRear].function = function;
+    thread_poll->taskQueue[thread_poll->queueRear].arg = arg;
+    /* 队列尾指针后移 */
+    thread_poll->queueRear = (thread_poll->queueRear + 1) % thread_poll->queueCapacity;
+    /* 队列大小+1 */
+    thread_poll->queueSize++;
+    /* 解锁 */
+    pthread_mutex_unlock(&thread_poll->mutex);
+
+    /* 唤醒消费者 */
+    pthread_cond_signal(&thread_poll->notEmpty);
+
+    return SUCCESS;
+}
+
 /* 线程池销毁 */
-int thread_poll_destroy(thread_poll_t *thread_poll);
-/* 线程池启动 */
-int thread_poll_start(thread_poll_t *thread_poll);
+int threadPollDestroy(thread_poll_t *thread_poll)
+{
+
+    thread_poll->shutdown = 1;
+    
+    /* 唤醒所有线程 */
+    pthread_cond_broadcast(&thread_poll->notEmpty);
+
+    return SUCCESS;
+}
+
--- threadPoll/threadPoll.h
+++ threadPoll/threadPoll.h
@@ -11,29 +11,47 @@ typedef struct task_t
     struct task_t *next;
     pthread_mutex_t mutex;
     pthread_cond_t cond;
+    
 }task_t;
 
 /* 线程池结构体 */
 typedef struct thread_poll_t 
 {
-    task_t *taskQueue;     // 任务队列
+    /* 任务队列 */
+    task_t *taskQueue;        // 任务队列
     int queueCapacity;        // 任务队列容量
     int queueSize;            // 任务队列大小
     int queueFront;           // 队列头
     int queueRear;            // 队列尾
 
-    pthread_t *threadID;   // 线程id数组
-    int maxSize;        // 最大线程数量
-    int minSize;        // 最小线程数量
+    /* 线程数组 */
+    pthread_t *threadID;      // 线程id数组
+    pthread_t managerID;      // 管理者线程id
+    int maxSize;              // 最大线程数量
+    int minSize;              // 最小线程数量
+    int busySize;             // 忙线程数量
+    int aliveSize;            // 存活线程数量
+    int exitSize;             // 退出线程数量
+    int shutdown;             // 线程池状态
+    
+
+    /* 互斥锁 */
+    pthread_mutex_t mutex;
+    pthread_cond_t notEmpty;
+    pthread_cond_t notFull;
+    pthread_mutex_t busyMutex;// 忙线程数量互斥锁
+    
 }
 thread_poll_t;
 
 /* 线程池初始化 */
-int thread_poll_init(thread_poll_t *thread_poll, int minSize, int maxSize, int queue_max_size);
+int threadPollInit(thread_poll_t *thread_poll, int minSize, int maxSize, int queueCapacity);
+/* 线程池添加任务 */
+int threadPollAddTask(thread_poll_t *thread_poll, void *(*function)(void *), void *arg);
 /* 线程池销毁 */
-int thread_poll_destroy(thread_poll_t *thread_poll);
+int threadPollDestroy(thread_poll_t *thread_poll);
 /* 线程池启动 */
-int thread_poll_start(thread_poll_t *thread_poll);
+int threadPollStart(thread_poll_t *thread_poll);
 
 
 #endif //__THREAD_POLL_H__
