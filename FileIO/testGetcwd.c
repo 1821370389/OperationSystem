c4766d5bf910a28b9b706d52b5d606385a2d3c7d
MUUWEI <1821370389@qq.com>
2024-02-02 15:41:16 +0800

    [修改] 历史遗留，干什么忘了

--- FileIO/testGetcwd.c
+++ FileIO/testGetcwd.c
@@ -11,9 +11,9 @@ int main(int argc, char *argv[])
 {
 
     char ret[BUFFER_SIZE] = {0};
-    char *ptr = getcwd(ret, BUFFER_SIZE - 1);
-    printf("%s\n", ret);
-    printf("%s\n", ptr);
+    char *ptr = getcwd(ret, BUFFER_SIZE - 1);   // 获取当前目录
+    printf("%s\n", ret);                        // 打印当前目录
+    printf("%s\n", ptr);                        // 打印
 
     return 0;
 }
--- FileIO/testTime.c
+++ FileIO/testTime.c
@@ -7,6 +7,20 @@
 /* 调试标记是否存在 */
 int g_debug = 0;
 
+FILE *log_fp = NULL;
+
+#define LOGPR(fmt, args...)             \
+    do                                  \
+    {                                   \
+        if(g_debug)                     \
+        {                               \
+            time_t now;                 \
+            struct tm *ptm = NULL;      \
+            now = time(NULL);            \
+            ptm = localtime(&now);       \
+            fprintf(log_fp, "file:(%s), func:(%s), line:(%d) %s " fmt "\n", __FILE__, __func__, __LINE__, asctime(ptm), ##args); \
+        }                               \
+    }while(0)
 /* 打印时间 */
 void printTime(time_t t)
 {
@@ -41,10 +55,27 @@ void log_init(const char *log_path)
     }
 #define DEBUF_FILE "./my_debug.log"
 
+    if((log_fp = fopen(DEBUF_FILE,"a") == NULL))
+    {
+        perror("fopen");
+        return;
+    }
+
+    now = time(NULL);
+    localtime(&now);
+
 }
 
 int main()
 {
+    log_init("./my_debug.log");
+
+    int count = 5;
+    while(count--)
+    {
+        LOGPR("count:%c\n",count);
+        sleep(1);
+    }
     struct tm *info = localtime(NULL);
     printf("%d\n", info->tm_year);
     printf("%d\n", info->tm_mon);
